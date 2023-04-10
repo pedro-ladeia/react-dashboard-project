b841371d2a6a13c8948cd0efb4b802d567586426
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-04-10 19:18:56 -0300

    Adding the navbar

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard page</div>} />
+              <Route path="/" element={<div>Prediction page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
