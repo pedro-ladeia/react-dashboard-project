cbe4cab92b06fe9d16ee9251835efc15e30bcdd3
Pedro <pedro.ladeia02@gmail.com>
2023-04-20 15:02:43 -0300

    created prediction page button link

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -11,21 +11,43 @@ const Navbar = (props: Props) => {
   const [selected, setSelected] = useState("dashboard");
   return (
     <FlexBetween mb=".25rem" p=".5rem 0rem" color={palette.grey[300]}>
+
       {/*Left side*/}
 
       <FlexBetween gap=".75rem">
-        <PixIcon sx={{ fontSize:"28px" }} />
+        <PixIcon sx={{ fontSize: "28px" }} />
         <Typography variant="h4" fontSize="16px" >Finanseer</Typography>
       </FlexBetween>
 
       {/*Right side */}
 
       <FlexBetween gap="2rem" >
-        <Box sx={{ "&:hover": {color: palette.primary[100]} }} ></Box>
-        <Link to="/" onClick={() => setSelected("dashboard")}  >
-          Dashboard
-        </Link>
-        <Box></Box>
+
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }} >
+          <Link
+            to="/" onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700]
+            }}
+          >
+            Dashboard
+          </Link>
+
+        </Box>
+
+
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }} >
+          <Link
+            to="/predictions" onClick={() => setSelected("Prediction")}
+            style={{
+              color: selected === "Prediction" ? "inherit" : palette.grey[700]
+            }}
+          >
+            Prediction
+          </Link>
+
+        </Box>
+
       </FlexBetween>
 
     </FlexBetween>
