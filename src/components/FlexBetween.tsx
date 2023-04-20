15e64c9f2ad46c0c3415291923c69e0535c0bfb5
Pedro <pedro.ladeia02@gmail.com>
2023-04-20 15:02:20 -0300

    Added some commnents

--- src/components/FlexBetween.tsx
+++ src/components/FlexBetween.tsx
@@ -1,7 +1,7 @@
-import { Box } from "@mui/material";
-import {styled} from "@mui/system";
+import { Box } from "@mui/material"; // A wrapper component with css utilites
+import {styled} from "@mui/system"; //Utility for style components
 
-const FlexBetween = styled(Box)({
+const FlexBetween = styled(Box)({ //Creating a component using both impots for stle
     display: "flex", 
     justifyContent: "space-between", 
     alignItems: "center"
