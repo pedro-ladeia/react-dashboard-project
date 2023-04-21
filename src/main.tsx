634a1c570a71599e76bba8b97fc5cfe105f62e6b
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-04-21 17:55:40 -0300

    Adding store redux config

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App'
 import './index.css'
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { api } from "@/states/api";
+
+export const store = configureStore({
+  reducer: {[api.reducerPath]: api.reducer},
+  middleware: (getDefault) => getDefault().concat(api.middleware)
+
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
