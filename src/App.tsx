378e50bd875aa07226bbff7b52cd79c7f2e0df04
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-04-21 17:54:11 -0300

    Adding the Dashboard scene on default rout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -15,8 +16,8 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>Dashboard page</div>} />
-              <Route path="/" element={<div>Prediction page</div>} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<div>Prediction page</div>} />
             </Routes>
           </Box>
         </ThemeProvider>
