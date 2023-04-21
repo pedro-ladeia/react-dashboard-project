6b807adac944335e500fb0fa19598215c77a3a48
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-04-21 17:47:43 -0300

    Adding some comments

--- src/components/FlexBetween.tsx
+++ src/components/FlexBetween.tsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material"; // A wrapper component with css utilites
 import {styled} from "@mui/system"; //Utility for style components
 
-const FlexBetween = styled(Box)({ //Creating a component using both impots for stle
+const FlexBetween = styled(Box)({ //Creating a component using both imports for style
     display: "flex", 
     justifyContent: "space-between", 
     alignItems: "center"
-})
+}) //Flex box  with space between
 
 
 export default FlexBetween;
