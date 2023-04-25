14819786ddd6328c085665210579f3379b9052e4
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-04-24 23:40:17 -0300

    switch the response type of query, void to GetkpiResponse array

--- src/states/api.ts
+++ src/states/api.ts
@@ -1,12 +1,13 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { GetKpisResponse } from './types';
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}),
     reducerPath: "main",
     tagTypes: ["Kpis"],
     endpoints: (build) => ({
-        getKpis: build.query<void, void>({
-            query: () => "kpi/kpis",
+        getKpis: build.query<Array<GetKpisResponse>, void>({
+            query: () => "/kpi",
             providesTags: ["Kpis"],
         }),
     }),
