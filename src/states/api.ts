6c5770ee51e10c3cebebe2545ae7ec895943e806
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-04-29 22:15:55 -0300

    Added build.query for products

--- src/states/api.ts
+++ src/states/api.ts
@@ -1,16 +1,20 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-import { GetKpisResponse } from './types';
+import { GetKpisResponse, GetProductsResponse } from './types';
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}),
     reducerPath: "main",
-    tagTypes: ["Kpis"],
+    tagTypes: ["Kpis", "Products"],
     endpoints: (build) => ({
         getKpis: build.query<Array<GetKpisResponse>, void>({
             query: () => "/kpi",
             providesTags: ["Kpis"],
         }),
+        getProducts: build.query<Array<GetProductsResponse>, void>({
+            query: () => "/product",
+            providesTags: ["Products"],
+        }),
     }),
 });
 
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery, useGetProductsQuery } = api;
