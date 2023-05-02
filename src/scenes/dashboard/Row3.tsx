c78a02527c79fc39bd0d6e5cee4338d524688b3a
Pedro Henrique de Melo Ladeia <pedro.ladeia02@gmail.com>
2023-05-02 20:16:18 -0300

    take the all data the row 3 needs

--- src/scenes/dashboard/Row3.tsx
+++ src/scenes/dashboard/Row3.tsx
@@ -1,15 +1,32 @@
 import DashboardBox from '@/components/DashboardBox'
+import { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } from '@/states/api'
 import React from 'react'
 
-type Props = {}
+const Row3 = () => {
+  const {data: kpiData} = useGetKpisQuery();
+  const {data: productData} = useGetProductsQuery();
+  const { data: transactionData } = useGetTransactionsQuery(); 
+  //console.log('data is: ', transactionData);
+  // Taking the all data the row 3 needs
 
-const Row3 = (props: Props) => {
   return (
     <>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <DashboardBox gridArea="g">
+        
+      </DashboardBox>
+
+      <DashboardBox gridArea="h">
+
+      </DashboardBox>
+
+      <DashboardBox gridArea="i">
+
+      </DashboardBox>
+
+      <DashboardBox gridArea="j">
+
+      </DashboardBox>
+
     </>
   )
 }
